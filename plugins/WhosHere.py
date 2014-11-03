9f13874a4c1c4702dcdb338fe75c22b10de3f236
Markus Meissner <markus@evigo.net>
2014-11-03 04:48:52 +0100

    minor

--- plugins/WhosHere.py
+++ plugins/WhosHere.py
@@ -12,11 +12,11 @@ __metaclass__ = type
 class WhosHere(AbstractPlugin):
     author = "meissna"
     react_to = {"public_command": re.compile(r"(?P<nick>.*)")}
-    provide = ["whoshere", "whois"]
+    provide = ["whoshere"]
     
     def react(self, data):
         data.chan << ", ".join(u.name for u in data.chan.users)
                         
     doc = { "whoshere" : ("whoshere", "Ich sage wer alles in dem Channel rumhängt"),
-            "whois" : ("whois <nick>", "Ich geb dir den Hostname von 'nick'") }
+            #"whois" : ("whois <nick>", "Ich geb dir den Hostname von 'nick'") }
     __doc__ = "Giving additional channel and user information"
